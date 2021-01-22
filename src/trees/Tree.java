675126c4c8d29790439d9c54747bbc2685e72e68
RamanSB <ramansb100@gmail.com>
2021-01-22 06:36:29 +0000

    Added find() method which will be used to locate a node in a tree containing a given key/data value

--- src/trees/Tree.java
+++ src/trees/Tree.java
@@ -1,4 +1,49 @@
 package trees;
 
+/**
+ * This class represents a Binary Search Tree. A binary search tree is a tree that consists of a collection of nodes,
+ * whom have at most 2 children (child nodes). Each node contains a piece of data (See the {Node.class}). A Node's left
+ * child must be of a lesser value than its' parent & the right child must contain data of greater value than its' parent.
+ *
+ * The Tree class enables us to access all nodes in the tree, we only require a single reference to the root node, from
+ * this node we can access all subsequent nodes using the node.leftChild, node.rightChild references.
+ *
+ * The tree class contains methods that allow us to:
+ *  - insert a node in to the tree
+ *  - find a node within the tree
+ *  - delete a node within the tree.
+ *
+ */
 public class Tree {
+
+    Node root = null;
+
+    Node insert(int data){
+
+    }
+
+
+    Node find(int data){
+        Node current = root;
+        while(current.data != data){
+            //If we are searching for a node with a value greater than the current nodes value - search subtree from the right child
+            if(data > current.data){
+                current = current.rightChild;
+            }
+            if(data < current.data){ ////If we are searching for a node with a value less than the current nodes value - search subtree from the left child
+                current = current.leftChild;
+            }
+            if(current == null){
+                return null; //Node cannot be found so we return null to indicate this
+            }
+        }
+        return current;
+    }
+
+    public static void main(String[] args){
+        Tree tree = new Tree();
+        Node rootNode = new Node(30);
+        tree.root = rootNode;
+    }
+
 }
