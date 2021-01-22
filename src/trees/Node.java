3f44e2b0d369d763c06566db71de8306ff7f4a12
RamanSB <ramansb100@gmail.com>
2021-01-22 06:26:39 +0000

    A class which represents a Node within a Binary Search Tree

---
+++ src/trees/Node.java
@@ -0,0 +1,24 @@
+package trees;
+
+/**
+ * This class represents a Node within a Tree data-structure.
+ * - A node may have at most 2 chlidren (or none in the case of a leaf node)
+ * - Each node has a reference to 2 possible children (node.leftChild, node.rightChild)
+ */
+public class Node {
+
+    int data; //We can create a separate class i.e. NodeData which will be an Object containing arbitrary data a node may represent.
+    Node leftChild;
+    Node rightChild;
+
+    Node(int data){
+        this.data = data;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+
+    @Override
+    public String toString(){
+        return "Node: " + this.data;
+    }
+}
