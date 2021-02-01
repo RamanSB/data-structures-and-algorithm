4c36f911f7b103b9abf847af8aa706b2846a0e5e
RamanSB <ramansb100@gmail.com>
2021-02-01 04:15:00 +0000

    kth to last element of linked list problem attempt - O(2n-k) = O(n)

--- crack-the-coding-interview/src/linkedlists/LinkedListSolutions.java
+++ crack-the-coding-interview/src/linkedlists/LinkedListSolutions.java
@@ -11,10 +11,12 @@ import java.util.Map;
  * 2) Iterate through the linked list keeping a reference on both the current link & the previous link.
  * 3) We will put each link we encounter in to our hashtable/map, however if the key already exists with a non-null value
  *    it is a duplicate and must be removed, we then set the previous.next = current.next, this 'cuts' out the duplicate link.
- * 4) We continue iteraing through the linked list until the current link is null.
+ * 4) We continue iterating through the linked list until the current link is null.
+ *
+ * Return kth to last element: Write an algorithm to find the kth to last element in a singly linked list.
  */
 
-public class RemoveDups {
+public class LinkedListSolutions {
 
     public static void main(String[] args){
         LinkedList linkedList = new LinkedList();
@@ -28,8 +30,9 @@ public class RemoveDups {
         linkedList.insertFirst(7);
         linkedList.insertFirst(3);
         System.out.println(linkedList);
-        System.out.println("Removing duplicates");
-        RemoveDups.removeDuplicates(linkedList);
+        //System.out.println("Removing duplicates");
+        //LinkedListSolutions.removeDuplicates(linkedList);
+        System.out.println(LinkedListSolutions.kthToLastElement(linkedList, 8));
         System.out.println(linkedList);
     }
 
@@ -48,5 +51,22 @@ public class RemoveDups {
             current = current.next;
         }
         return linkedList;
+    }
+
+    static LinkedList.Link kthToLastElement(LinkedList linkedList, int k){
+        int counter = 0;
+        LinkedList.Link current = linkedList.first;
+        while(current!=null){
+            counter++;
+            current = current.next;
+        }
+        if(k>counter){
+            throw new IndexOutOfBoundsException("List is of size " + counter + ". " + k +"th to last element does not exist.");
+        }
+        current = linkedList.first;
+        for(int i=0; i<counter-k; i++){
+            current = current.next;
+        }
+        return current;
     }
 }
