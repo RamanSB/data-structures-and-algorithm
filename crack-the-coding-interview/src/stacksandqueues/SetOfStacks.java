f796a8a0b9b1a6c465409e7df8fea592ac999f48
RamanSB <ramansb100@gmail.com>
2021-02-06 21:35:43 +0000

    additional popAt question complete used O(n) space

--- crack-the-coding-interview/src/stacksandqueues/SetOfStacks.java
+++ crack-the-coding-interview/src/stacksandqueues/SetOfStacks.java
@@ -12,12 +12,14 @@ public class SetOfStacks {
     int noOfStacksInSet;
     int stackIndex;
     int top = -1;
+    int[] stackTops; //Used for popAt
 
     SetOfStacks(int threshold, int noOfStacksInSet){
         this.threshold = threshold;
         this.noOfStacksInSet = noOfStacksInSet;
         this.stackMainArray = new int[noOfStacksInSet][threshold];
         this.stackIndex = -1;
+        this.stackTops = new int[noOfStacksInSet];
     }
 
     void push(int elem){
@@ -28,6 +30,7 @@ public class SetOfStacks {
                 throw new ArrayIndexOutOfBoundsException("All stacks in set are full");
             }
         }
+        stackTops[stackIndex] = (top+1);
         stackMainArray[stackIndex][++top] = elem;
     }
 
@@ -44,10 +47,18 @@ public class SetOfStacks {
             throw new ArrayIndexOutOfBoundsException("All stacks are empty");
         }
         int poppedItem = stackMainArray[stackIndex][top--];
+        stackTops[stackIndex] = (top-1);
         if(stackIndex == 0 & top==-1){
             stackIndex--;
         }
         return poppedItem;
+    }
+
+    int popAt(int index){
+        if(stackTops[index] == -1){
+            throw new ArrayIndexOutOfBoundsException("Stack at index " + index + " is empty.");
+        }
+        return stackMainArray[index][stackTops[index]--];
     }
 
     @Override
@@ -67,59 +78,38 @@ public class SetOfStacks {
     }
 
     public static void main(String[] args){
-        SetOfStacks setOfStacks = new SetOfStacks(4, 3);
+        SetOfStacks setOfStacks = new SetOfStacks(3, 4);
+        setOfStacks.push(2);
+        setOfStacks.push(2);
+        setOfStacks.push(2);
+        setOfStacks.push(2);
+        System.out.println(setOfStacks);
+        setOfStacks.pop();
+        setOfStacks.pop();
+        setOfStacks.pop();
+        setOfStacks.pop();
+        System.out.println(setOfStacks);
+        setOfStacks.push(4);
+        setOfStacks.push(4);
+        setOfStacks.push(4);
+        setOfStacks.push(4);
+        System.out.println(setOfStacks);
         setOfStacks.push(5);
-        setOfStacks.push(8);
+        setOfStacks.push(5);
+        setOfStacks.push(6);
         System.out.println(setOfStacks);
-        setOfStacks.push(9);
-        setOfStacks.push(12);
-        setOfStacks.push(13);
+        setOfStacks.push(6);
+        setOfStacks.push(6);
+        setOfStacks.push(67);
         System.out.println(setOfStacks);
-        setOfStacks.push(21);
-        setOfStacks.push(24);
+        setOfStacks.push(67);
+        setOfStacks.push(67);
         System.out.println(setOfStacks);
-        System.out.println(setOfStacks.peek());
-        setOfStacks.push(24);
-        setOfStacks.push(84);
-        setOfStacks.push(15);
-        setOfStacks.push(96);
-        System.out.println(setOfStacks);
-        setOfStacks.push(1);
-        System.out.println(setOfStacks);
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks);
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks);
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks);
-        setOfStacks.push(53);
-        setOfStacks.push(53);
-        setOfStacks.push(12);
-        setOfStacks.push(53);
-        setOfStacks.push(13);
-        setOfStacks.push(132);
-        System.out.println(setOfStacks);
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks);
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks.pop());
-        System.out.println(setOfStacks);
-
-
+        System.out.println(setOfStacks.popAt(3));
+        System.out.println(setOfStacks.popAt(2));
+        System.out.println(setOfStacks.popAt(1));
+        System.out.println(setOfStacks.popAt(1));
+        System.out.println(setOfStacks.popAt(2));
     }
 
 }
